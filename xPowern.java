873a242c496adb53a1736ab86f28bd6250001f3f
noor <nooruddinansary@gmail.com>
2023-12-09 02:35:46 +0530

    1st program

---
+++ xPowern.java
@@ -0,0 +1,20 @@
+
+
+public class xPowern {
+    public static int power(int x, int n) {
+        if(n==0){
+            return 1;
+        }
+        if(x==0){
+            return 0;
+        }
+        return x*power(x, n-1);
+        
+    }
+    public static void main(String[] args) {
+        int x=2, n=5;
+        int s=power(x, n);
+        System.out.println(s);
+    }
+
+}
